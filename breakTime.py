0a0b317baf9c2a379d2047b751be390f6ec41631
Sruc73 <michaelmares@outlook.com>
2018-03-02 15:24:58 +0100

    Created a loop that executes the code three times

--- breakTime.py
+++ breakTime.py
@@ -1,5 +1,9 @@
 import webbrowser
 import time
 
-time.sleep(10)
-webbrowser.open("https://www.youtube.com/watch?v=WfPu9Jrcpuk")
+breakTime = 0
+
+while breakTime < 3:
+    time.sleep(10)
+    webbrowser.open("https://www.youtube.com/watch?v=WfPu9Jrcpuk")
+    breakTime += 1
